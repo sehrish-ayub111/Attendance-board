b7f35a21f84e9ea39037c0b17c05f59efdbf599b
Apna GitHub Username <Apni GitHub Email>
2026-07-09 17:50:58 +0500

    Fix API URL

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql2/promise'
 
 export const pool = mysql.createPool({
-    host: 'localhost',
+    host: '/api/',
     user: 'root',
     password: 'attendance@12',
     database: 'attendance_db',
--- src/apiClients.js
+++ src/apiClients.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:4000/api'
+const API_BASE = '/api'
 async function request(path, options = {}) {
   const res = await fetch(`${API_BASE}${path}`, {
     headers: { 'Content-Type': 'application/json' },
